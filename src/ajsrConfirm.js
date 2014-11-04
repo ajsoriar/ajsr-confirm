9a0f61951a67e91c0326ef79a89f947fbea7967d
ajsoriar <ajsoriar@gmail.com>
2014-11-04 18:51:42 +0100

    add anonymous function wrapper

--- src/ajsrConfirm.js
+++ src/ajsrConfirm.js
@@ -1,81 +1,86 @@
-define([
-  'jquery',
-  'underscore',
-  'handlebars',
-  'backbone',
-  './config',
-  'text!/js/components/confirmDialogBox/templates/template.html'
-], function(
-  $,
-  _,
-  Handlebars,
-  Backbone,
-  configuration,
-  componentTemplate
-) {
-  'use strict';
-  var Component = function(options) {
+(function($) {
 
-    var componentOptions = _.extend(JSON.parse(JSON.stringify(configuration)), options || {});
 
-    var ComponentView = Backbone.View.extend({
-      events: {
-        'click #btnAccept': 'onAcept',
-        'click #btnCancel': 'onCancel'
-      },
-      initialize: function() {
-        this.render();
-      },
-      render: function() {
 
-        $(this.$el).append('<div id="dlgContainer"></div>');
-        this.$el = this.$el.find("#dlgContainer");
-        var template = Handlebars.compile(componentTemplate);
-        template = $(template({
-          id: null,
-          dlgBarText: componentOptions.dlgBarText,
-          dlgText: componentOptions.dlgText
-        }));
+    //title, 
+    //text, 
+    //onCancel
+    //onConfirm, 
+    //cancelButtonText, 
+    //confirmButtonText, 
+    //dlgClass
 
-        this.$el.html(template); 
-        this.centerOnScreen();
-      },
-      centerOnScreen: function () {
-        var dlg = this.$el.find("#dialog");
-        dlg.css({'position': 'fixed'});
-        var positionX = $(window).width()/2 - dlg.width()/2 +"px";
-        var positionY = $(window).height()/2 - dlg.height()/2 +"px";   
-        dlg.css({'left': positionX});
-        dlg.css({'top': positionY});  
-      },
-      destroyComponent: function() {
-        this.remove();
-      },
-      onAcept: function() {
-        fnOnAcept();
-        this.destroyComponent();
-      },
-      onCancel: function() {
-        fnOnCancel();
-        this.destroyComponent();
-      }
-    });
+    $.ajsrConfirm = function(options, e) {
 
-    var fnOnAcept;
-    if (componentOptions.onAcept !== null) {
-      fnOnAcept = componentOptions.onAcept;
-    }
+        //'use strict';
+        //var Component = function(options) {
 
-    var fnOnCancel;
-    if (componentOptions.onCancel !== null) {
-      fnOnCancel = componentOptions.onCancel;
-    }
+        var componentOptions = _.extend(JSON.parse(JSON.stringify(configuration)), options || {});
 
-    var view = new ComponentView({
-      el: $(document.body)
-    });
+        var ComponentView = Backbone.View.extend({
+            events: {
+                'click #btnAccept': 'onAcept',
+                'click #btnCancel': 'onCancel'
+            },
+            initialize: function() {
+                this.render();
+            },
+            render: function() {
 
-  };
+                $(this.$el).append('<div id="dlgContainer"></div>');
+                this.$el = this.$el.find("#dlgContainer");
+                var template = Handlebars.compile(componentTemplate);
+                template = $(template({
+                    id: null,
+                    dlgBarText: componentOptions.dlgBarText,
+                    dlgText: componentOptions.dlgText
+                }));
 
+                this.$el.html(template);
+                this.centerOnScreen();
+            },
+            centerOnScreen: function() {
+                var dlg = this.$el.find("#dialog");
+                dlg.css({ 'position': 'fixed' });
+                var positionX = $(window).width() / 2 - dlg.width() / 2 + "px";
+                var positionY = $(window).height() / 2 - dlg.height() / 2 + "px";
+                dlg.css({ 'left': positionX });
+                dlg.css({ 'top': positionY });
+            },
+            destroyComponent: function() {
+                this.remove();
+            },
+            onAcept: function() {
+                fnOnAcept();
+                this.destroyComponent();
+            },
+            onCancel: function() {
+                fnOnCancel();
+                this.destroyComponent();
+            }
+        });
+
+        var fnOnAcept;
+        if (componentOptions.onAcept !== null) {
+            fnOnAcept = componentOptions.onAcept;
+        }
+
+        var fnOnCancel;
+        if (componentOptions.onCancel !== null) {
+            fnOnCancel = componentOptions.onCancel;
+        }
+
+        var view = new ComponentView({
+            el: $(document.body)
+        });
+
+    };
+
+});
+/*
   return Component;
 });
+
+
+
+*/
