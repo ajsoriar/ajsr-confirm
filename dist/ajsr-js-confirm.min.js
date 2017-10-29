f45765f38512c3ff49b31a1fdf89aa21dc72afe5
asr <ajsoriar@gmail.com>
2017-10-30 00:54:30 +0100

    Upgrade dist files

--- dist/ajsr-js-confirm.min.js
+++ dist/ajsr-js-confirm.min.js
@@ -1,7 +1,7 @@
 /**
  * ajsr-confirm
  * javascript confirm dialog box including several fun css templates.
- * @version v1.1.1 - 2017-10-29
+ * @version v1.2.0 - 2017-10-29
  * @link https://github.com/ajsoriar/ajsr-confirm
  * @author Andres J. Soria R. <ajsoriar@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
